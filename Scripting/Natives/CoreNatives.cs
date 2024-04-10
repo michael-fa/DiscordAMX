9930ae6cd578cd7f7e0c546624f32c441852aff5
michael-fa <michixx.bat@gmail.com>
2024-04-11 00:19:47 +0200

    demo test

--- Scripting/Natives/CoreNatives.cs
+++ Scripting/Natives/CoreNatives.cs
@@ -108,20 +108,20 @@ namespace discordamx.Scripting.Natives
                                     {
                                         p.AMX.Push(args1[count].AsIntPtr());
                                         count--;
-                                        continue;
+                                        break;
                                     }
                                 case 'f':
                                     {
                                         p.AMX.Push((float)args1[count].AsCellPtr().Get().AsFloat());
                                         count--;
-                                        continue;
+                                        break;
                                     }
 
                                 case 's':
                                     {
                                         Cells.Add(p.AMX.Push(args1[count].AsString()));
                                         count--;
-                                        continue;
+                                        break;
                                     }
                             }
                         }
@@ -129,13 +129,13 @@ namespace discordamx.Scripting.Natives
                         count = (args1.Length - 1);
                         p.Execute();
 
-                    }
 
-                    foreach (CellPtr cell in Cells)
-                    {
-                        p.AMX.Release(cell);
+                        foreach (CellPtr cell in Cells)
+                        {
+                            p.AMX.Release(cell);
+                        }
+                        GC.Collect();
                     }
-                    GC.Collect();
                 }
             }
             catch (Exception ex)
@@ -172,17 +172,50 @@ namespace discordamx.Scripting.Natives
 
         public static int SetTimerEx(AMX amx1, AMXArgumentList args1, Script caller_script)
         {
-            if(args1.Length < 5) return 1;
+            if (args1.Length < 5)
+                return 1; // Return an error code or handle the case where there are insufficient arguments
 
             try
             {
-                ScriptTimer timer = new ScriptTimer(args1[1].AsInt32(), (bool)Convert.ToBoolean(args1[2].AsInt32()), args1[0].AsString(), caller_script, args1[3].AsString(), args1);
+                int duration = args1[1].AsInt32(); // Assuming duration is the first argument
+                bool repeat = Convert.ToBoolean(args1[2].AsInt32()); // Assuming repeat is the second argument
+                string callback = args1[0].AsString(); // Assuming callback is the third argument
+                string format = args1[3].AsString(); // Assuming format is the fourth argument
+
+                // Extracting arguments based on the format string
+                object[] parsedArgs = new object[format.Length];
+                int argIndex = 4; // Start from index 4 of AMXArgumentList
+                for (int i = 0; i < format.Length; i++)
+                {
+                    switch (format[i])
+                    {
+                        case 'i':
+                            parsedArgs[i] = args1[argIndex].AsInt32(); // Assuming integers start from index 4
+                            argIndex++;
+                            break;
+                        case 'f':
+                            parsedArgs[i] = args1[argIndex].AsFloat(); // Assuming floats start from index 5
+                            argIndex++;
+                            break;
+                        case 's':
+                            parsedArgs[i] = args1[argIndex].AsString(); // Assuming strings start from index 6
+                            argIndex++;
+                            break;
+                        default:
+                            // Handle unsupported format specifier
+                            break;
+                    }
+                }
+
+                // Now call the ScriptTimer function with parsed arguments
+                new ScriptTimer(duration, repeat, callback, caller_script, format, parsedArgs);
+                return 1; 
             }
             catch (Exception ex)
             {
                 Program.m_Logger.Exception(ex, caller_script);
+                return -1; // Return an error code if an exception occurs
             }
-            return (Program.m_ScriptTimers.Count);
         }
 
         public static int KillTimer(AMX amx1, AMXArgumentList args1, Script caller_script)
--- Scripting/ScriptTimer.cs
+++ Scripting/ScriptTimer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -18,67 +19,48 @@ namespace discordamx.Scripting
         int m_msWait;
         string m_Func;
         Script m_ParentScript;
-        System.Threading.Timer m_Timer;
+        System.Timers.Timer m_Timer;
         AMXPublic m_AMXCallback;
         public string m_ArgFrmt;
-        public AMXArgumentList m_Args;
-        public object[] tracked_objects;
+        public object[] m_Args;
+        private object lockObject;
 
 
-        public ScriptTimer(int interval, bool rep, string funcCall, Script arg_parent_Script, string _ArgsFrm, AMXArgumentList _args)
+        public ScriptTimer(int interval, bool rep, string funcCall, Script arg_parent_Script, string _ArgsFrm, object[] _args)
         {
             m_ParentScript = arg_parent_Script;
-            m_AMXCallback = m_ParentScript.m_Amx.FindPublic(funcCall);
-            if (m_AMXCallback == null)
-            {
-                return;
-            }
+           
+
             m_msWait = interval;
+            Console.WriteLine(funcCall + " has to be called nigga.");
             m_Func = funcCall;
             m_Active = true;
             m_Repeat = rep;
             m_ArgFrmt = _ArgsFrm;
             m_Args = _args;
+            lockObject = new object();
 
-
-            int count = 1;
-            foreach (char x in Utils.Scripting.Reverse(m_ArgFrmt).ToCharArray())
+            m_AMXCallback = m_ParentScript.m_Amx.FindPublic(funcCall);
+            if (m_AMXCallback == null)
             {
-                switch (x)
-                {
-                    case 'i':
-                        {
-                            tracked_objects[count] = m_Args[count].AsIntPtr();
-                            count--;
-                            continue;
-                        }
-                    case 'f':
-                        {
-                            tracked_objects[count] = (float)m_Args[count].AsCellPtr().Get().AsFloat();
-                            count--;
-                            continue;
-                        }
-                    case 's':
-                        {
-                            tracked_objects[count] = m_Args[count].AsString();
-                            count--;
-                            continue;
-                        }
-                }
-                count++;
+                return;
             }
 
-
+            int count = m_ArgFrmt.Length - 1;
+            char[] reversedFormat = Utils.Scripting.Reverse(m_ArgFrmt).ToCharArray();
+            List<CellPtr> Cells = new List<CellPtr>();
 
             Program.m_ScriptTimers.Add(this);
             this.ID = Program.m_ScriptTimers.Count;
 
-            System.Threading.TimerCallback TimerDelegate =
-            new System.Threading.TimerCallback(OnTimedEvent);
 
-
-            m_Timer = new System.Threading.Timer(TimerDelegate, null, 200, 100);
+            //m_Timer = new System.Threading.Timer(TimerDelegate, null, m_msWait, m_msWait);
+            m_Timer = new System.Timers.Timer(m_msWait);
+            m_Timer.Elapsed += OnTimedEvent;
+            m_Timer.AutoReset = m_Repeat;
+            m_Timer.Start();
             Program.m_Logger.Debug("Initialised Script-Timer (\"" + m_Func + "\") !", arg_parent_Script);
+            return;
         }
 
         public ScriptTimer(int interval, bool rep, string funcCall, Script arg_parent_Script)
@@ -93,90 +75,106 @@ namespace discordamx.Scripting
             m_Func = funcCall;
             m_Active = true;
             m_Repeat = rep;
-
+            lockObject = new object();
 
 
             Program.m_ScriptTimers.Add(this);
             this.ID = Program.m_ScriptTimers.Count;
 
-            System.Threading.TimerCallback TimerDelegate =
-            new System.Threading.TimerCallback(OnTimedEvent!);
-
-
-            m_Timer = new System.Threading.Timer(TimerDelegate, null, m_msWait, m_msWait);
+            m_Timer = new System.Timers.Timer(m_msWait);
+            m_Timer.Elapsed += OnTimedEvent;
+            m_Timer.AutoReset = m_Repeat;
+            m_Timer.Start();
 
             Program.m_Logger.Debug("Initialised Script-Timer (\"" + m_Func + "\") !", arg_parent_Script);
         }
 
-        public void OnTimedEvent(Object state)
+        public void OnTimedEvent(object source, ElapsedEventArgs e)
         {
-            Console.WriteLine("LOOP");
-            try
+            lock (lockObject)
             {
-                m_AMXCallback = m_ParentScript.m_Amx.FindPublic(m_Func);
-                List<CellPtr> _list = new List<CellPtr>();
-                int count = (m_Args.Length - 1);
-                Console.WriteLine("Lop, " + m_Args.Length + ", " +  m_ArgFrmt.Length);
-                if (m_Args.Length != m_ArgFrmt.Length + 4)
+                Console.WriteLine("LOOP");
+                try
                 {
-                    Console.WriteLine("LOOP2");
-                    m_AMXCallback.AMX.RaiseError(AMXError.ParameterError);
-                    return;
-                }
-                Console.WriteLine("LOOP3");
-                if (count == 3) //the end of args passed
-                {
-                    m_AMXCallback.Execute();
-                    Program.m_Logger.Debug("Script-Timer invoked \"" + m_Func + "\"", m_ParentScript);
-                    return;
-                }
-                Console.WriteLine("LOOP4");
+                    m_AMXCallback = m_ParentScript.m_Amx.FindPublic(m_Func);
+                    if (m_AMXCallback == null) { return; }
+                    List<CellPtr> _list = new List<CellPtr>();
+                    //int count = (m_Args.Length - 1);
 
-                foreach (char x in Utils.Scripting.Reverse(m_ArgFrmt).ToCharArray())
-                {
-                    switch (x)
+                    if (m_Args == null) goto justcall;
+                    if (m_Args.Length != m_ArgFrmt.Length)
                     {
-                        case 'i':
-                            {
-                                m_AMXCallback.AMX.Push((int)tracked_objects[count]);
-                                count--;
-                                continue;
-                            }
-                        case 'f':
-                            {
-                                m_AMXCallback.AMX.Push((float)tracked_objects[count]);
-                                count--;
-                                continue;
-                            }
-                        case 's':
-                            {
-                                _list.Add(m_AMXCallback.AMX.Push((string)tracked_objects[count]));
-                                count--;
-                                continue;
-                            }
+                        Console.WriteLine("LOOP2");
+                        m_AMXCallback.AMX.RaiseError(AMXError.ParameterError);
+                        return;
                     }
-                }
-                m_AMXCallback.Execute();
+                    Console.WriteLine("LOOP3");
+                    if (m_Args.Length == 3) //the end of args passed
+                    {
+                        m_AMXCallback.Execute();
+                        Program.m_Logger.Debug("Script-Timer invoked \"" + m_Func + "\"", m_ParentScript);
+                        return;
+                    }
+                    Console.WriteLine("Lop, " + m_Args.Length + ", " + m_ArgFrmt.Length);
+                    Console.WriteLine("LOOP4");
 
-                foreach (CellPtr cell in _list)
+                    for (int i = 0; i < m_ArgFrmt.Length; i++)
+                    {
+                        switch (m_ArgFrmt[i])
+                        {
+                            case 'i':
+                                {
+                                    m_AMXCallback.AMX.Push((int)m_Args[i]);
+                                    break;
+                                }
+                            case 'f':
+                                {
+                                    m_AMXCallback.AMX.Push((float)m_Args[i]);
+                                    break;
+                                }
+                            case 's':
+                                {
+                                    _list.Add(m_AMXCallback.AMX.Push((string)m_Args[i]));
+                                    break;
+                                }
+                        }
+                    }
+
+                    justcall:
+                    // Assuming m_AMXCallback is an instance of a class and Execute is a method of that class.
+                    Type amxCallbackType = m_AMXCallback.GetType();
+                    MethodInfo executeMethod = amxCallbackType.GetMethod("Execute");
+
+                    if (executeMethod != null)
+                    {
+                        // Optionally, create an instance of the class if the method is not static.
+                        // If m_AMXCallback is already an instance, skip this step.
+                        // object instance = Activator.CreateInstance(amxCallbackType);
+
+                        // Invoke the method.
+                        executeMethod.Invoke(m_AMXCallback, null);
+                    }
+                    else
+                    {
+                        // Method not found.
+                        Console.WriteLine("Execute method not found.");
+                    }
+
+
+                    foreach (CellPtr cell in _list)
+                    {
+                        m_AMXCallback.AMX.Release(cell);
+                    }
+                    GC.Collect();
+
+                    Program.m_Logger.Debug("Script-TimerEx invoked \"" + m_Func + "\"", m_ParentScript);
+                }
+                catch (Exception ex)
                 {
-                    m_AMXCallback.AMX.Release(cell);
+                    Program.m_Logger.Exception(ex, m_ParentScript);
                 }
-                GC.Collect();
-
-                Program.m_Logger.Debug("Script-TimerEx invoked \"" + m_Func + "\"", m_ParentScript);
-            }
-            catch (Exception ex)
-            {
-                Program.m_Logger.Exception(ex, m_ParentScript);
             }
 
-            //No repeating?
-            if (!m_Repeat)
-            {
-                m_Timer.Change(Timeout.Infinite, Timeout.Infinite);
-                this.m_Active = false;
-            }
         }
 
 
@@ -186,7 +184,7 @@ namespace discordamx.Scripting
             if (!this.m_Active) return false;
 
 
-            m_Timer.Change(Timeout.Infinite, Timeout.Infinite);
+            m_Timer.Stop();
             this.m_Active = false;
             this.m_ArgFrmt = "";
 
